e6d7c7c91240d9311f5936759b5b36fa061cd6e6
John Coleman <jbcolemanjr@gmail.com>
2023-11-17 15:22:40 -0500

    component structure established

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BillList from './pages/BillList';
+import BillDetail from './pages/BillDetail';
+import UserProfile from './pages/UserProfile';
+import CreateBill from './pages/CreateBill';
+import EditProfile from './pages/EditProfile';
+
+const App = () => {
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <main>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/register" component={Register} />
+                        <Route path="/bills" component={BillList} />
+                        <Route path="/bill/:id" component={BillDetail} />
+                        <Route path="/user/:id" component={UserProfile} />
+                        <Route path="/create-bill" component={CreateBill} />
+                        <Route path="/edit-profile" component={EditProfile} />
+                        {/* Add other routes here */}
+                    </Switch>
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
 
 export default App;
+
--- server/app.py
+++ server/app.py
@@ -48,6 +48,31 @@ def create_user():
 
     return jsonify({'message': 'New user created!'})
 
+@app.route('/user/<int:user_id>', methods=['GET'])
+def user_profile(user_id):
+    user = User.query.get_or_404(user_id)
+    user_data = {'username': user.username, 'firstname': user.firstname, 'lastname': user.lastname, 'email': user.email}
+    return jsonify({'user': user_data})
+
+@app.route('/user/<int:user_id>', nmethods=['PATCH'])
+@token_required
+def update_user_profile(current_user, user_id):
+    if current_user.id != user_id:
+        return jsonify({'message': 'Permission denied'}), 403
+    
+    user = User.query.get_or_404(user_id)
+    data = request.get_json()
+    user.email = data.get('email', user.email)
+    user.firstname = data.get('firstname', user.firstname)
+    user.middlename = data.get('middlename', user.middlename)
+    user.lastname = data.get('lastname', user.lastname)
+    user.suffix = data.get('suffix', user.suffix)
+    user.username = data.get('username', user.username)
+    user.password = data.get('password', user.password)
+    
+    db.session.commit()
+    return jsonify('message': 'User profile updated')
+
 @app.route('/login', methods=['POST'])
 def login_user():
     auth = request.authorization
@@ -91,6 +116,66 @@ def revoke_delegate(current_user):
     db.session.commit()
 
     return jsonify({'message': 'You have revoked your vote delegation.'})
+
+@app.route('/bill', methods=['GET'])
+def list_bills():
+    bills = Bill.query.all()
+    bill_data = [{'id': bill.id, 'title': bill.title, 'summary': bill.summary, 'content': bill.content} for bill in bills]
+    return jsonify({'bills': bill_data})
+
+@app.route('/bill', methods=['POST'])
+@token_required
+def create_bill(current_user):
+    data = request.get_json()
+    new_bill = Bill(title=data['title'], content=data['content'], uploaded_by=current_user.id)
+    #call OpenAI summary
+    db.session.add(new_bill)
+    db.session.commit()
+
+    return jsonify({'message': 'New bill created!'})
+
+@app.route('/bill/<int:bill_id>', methods=['GET'])
+def get_bill(bill_id):
+    bill = Bill.query.filter_by(id=bill_id).first()
+
+    if not bill:
+        return jsonify({'message': 'No bill found!'})
+    
+    bill_data = {'title': bill.title, 'summary': bill.summary, 'content': bill.content, 'uploader': bill.uploader.username}
+    return jsonify({'bill': bill_data})
+
+@app.route('/bill/<int:bill_id>', methods=['PATCH'])
+@token_required
+def update_bill(current_user, bill_id):
+    bill = Bill.query.get_or_404(bill_id)
+    if bill.uploaded_by != current_user.id:
+        return jsonify({'message': 'Permission denied'}), 403
+    
+    data = request.get_json()
+    bill.title = data.get('title', bill.title)
+    bill.content = data.get('content', bill.content)
+    db.session.commit()
+    return jsonify({'message': 'Bill updated successfully!'})
+
+@app.route('/bill/<int:bill_id>', methods=['DELETE'])
+@token_required
+def delete_bill(current_user, bill_id):
+    bill = Bill.query.get_or_404(bill_id)
+    if bill.uploaded_by != current_user.id:
+        return jsonify({'message': 'Permission denied'}), 403
+    
+    db.session.delete(bill)
+    db.session.commit()
+    return jsonify({'message': 'Bill deleted successfully'})
+
+@app.route('/bill/<int:bill_id>/results', methods=['GET'])
+def bill_results(bill_id):
+    bill = Bill.query.get_or_404(bill_id)
+    votes = Vote.query.filter_by(bill_id=bill.id).all()
+    results = {'Yes': 0, 'No': 0, 'Abstain': 0}
+    for vote in votes:
+        results[vote.vote_type] += 1
+    return jsonify({'results': results})
 
 @app.route('/vote', methods=['POST'])
 @token_required
